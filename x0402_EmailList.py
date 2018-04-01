a8d39cddae4a9b4364387630c4055a67a053192b
Mike Kramer <31527153+mkramer45@users.noreply.github.com>
2018-04-01 10:57:05 -0400

    x
    x

---
+++ x0402_EmailList.py
@@ -0,0 +1,15 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import sqlite3
+import sys
+#sys.setdefaultencoding('utf8')
+
+conn = sqlite3.connect('StriveDB2.db')
+cursor = conn.cursor()
+cur5 = cursor.execute('SELECT email FROM Sendr_Usr WHERE RadioB = "Funny"')
+info5 = cur5.fetchall()
+
+
+print(info5)
+
