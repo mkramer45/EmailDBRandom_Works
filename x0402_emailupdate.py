a698c38d8bfcc0f3f6e54bcc1bbb23ba579ae72f
Mike Kramer <31527153+mkramer45@users.noreply.github.com>
2018-04-01 10:18:45 -0400

    X
    XC

---
+++ x0402_emailupdate.py
@@ -0,0 +1,43 @@
+import smtplib
+from email.MIMEMultipart import MIMEMultipart
+from email.MIMEText import MIMEText
+import sqlite3
+import sys
+#sys.setdefaultencoding('utf8')
+
+conn = sqlite3.connect('StriveDB2.db')
+cursor = conn.cursor()
+cur2 = cursor.execute('SELECT msg, RecordID FROM Motivational ORDER BY RANDOM() LIMIT 1;')
+info2 = cur2.fetchone()
+
+#msg
+j = info2[0]
+#recordID
+k = info2[1]
+
+msg = MIMEMultipart()
+msg['From'] = 'mkramer265@gmail.com'
+msg['To'] = 'mkramer789@gmail.com'
+msg['Subject'] = 'Motivational'
+message = j
+msg.attach(MIMEText(message))
+
+mailserver = smtplib.SMTP('smtp.gmail.com',587)
+# identify ourselves to smtp gmail client
+mailserver.ehlo()
+# secure our email with tls encryption
+mailserver.starttls()
+# re-identify ourselves as an encrypted connection
+mailserver.ehlo()
+mailserver.login('mkramer265@gmail.com', 'Celtics123')
+
+mailserver.sendmail('mkramer265@gmail.com','mkramer789@gmail.com',msg.as_string())
+
+mailserver.quit()
+
+connx = sqlite3.connect('StriveDB2.db')
+cursorx = connx.cursor()
+curx = cursorx.execute("INSERT INTO RIDX VALUES (?)", (k,))
+connx.commit()
+cursorx.close()
+connx.close()
